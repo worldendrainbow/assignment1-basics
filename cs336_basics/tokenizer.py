01b82262a771cd61e320279f6303aaec4a2b6c69
Kai_Mac <kaiadmin@KaiMacMacBook-Air.local>
2026-08-11 13:17:51 +0800

    0811-1

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -12,8 +12,8 @@ class Tokenizer:
         self.merges=merges
         self.special_tokens=special_tokens
         self.vocab_inv={}
-        for key,v in vocab:
-            self.vocab_inv[key]=v
+        for key,v in vocab.items():
+            self.vocab_inv[v]=key
     
     @classmethod
     def from_files(cls,
@@ -29,11 +29,17 @@ class Tokenizer:
     
     def encode(self,text:str)->list[int]:
         PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        special_pattern = '|'.join(re.escape(token) for token in self.special_tokens)
-
         tokens=[]
-        for segment in re.split(special_pattern, text):
-            for match in re.finditer(PAT, segment):             
+
+        if self.special_tokens is not None:
+            sorted_spec=sorted(self.special_tokens,key=len,reverse=True)
+            special_pattern = '('+'|'.join(re.escape(token) for token in sorted_spec)+')'
+            for segment in re.split(special_pattern, text):
+                for match in re.finditer(PAT, segment):
+                    tokens.append(tuple(bytes([b]) for b in match.group(0).encode("utf-8")))
+        else:
+            segment=text
+            for match in re.finditer(PAT, segment):
                 tokens.append(tuple(bytes([b]) for b in match.group(0).encode("utf-8")))
 
         for merge in self.merges:
@@ -42,19 +48,37 @@ class Tokenizer:
                 i=0
                 while i < len(tokens[now])-1:
                     if tokens[now][i]==merge[0] and tokens[now][i+1]==merge[1]:
-                        new_token=new_token+tuple(merge[0]+merge[1])
+                        new_token=new_token+(merge[0]+merge[1],)
                         i+=2
                     else:
+                        new_token=new_token +(tokens[now][i],)
                         i+=1
+                if i==len(tokens[now])-1:
+                    new_token=new_token+(tokens[now][-1],)
                 tokens[now]=new_token
-                
-        return [self.vocab[t] for token in tokens for t in token]
+        # for token in tokens:
+        #     for t in token:
+        #         print(t)
+        # print((tokens))
+        return [self.vocab_inv[t] for token in tokens for t in token]
 
     
     def encode_iterable(self,iterable:Iterable[str])->Iterator[int]:
-
-        raise NotImplementedError
+        for text in iterable:
+            yield from self.encode(text)
 
     def decode(self,ids:list[int])->str:
+        ret=bytes([])
+        for id in ids:
+            ret=ret+self.vocab[id]
+        return ret.decode("utf-8")
+        # return [self.vocab[id].decode("utf-8") for id in ids]
+    
 
-        raise NotImplementedError
+if __name__=='__main__':
+    vocab_filepath='data/processed/output/vocab.pkl'
+    merges_filepath='data/processed/output/merge_pair.pkl'
+    special_tokens=['<|endoftext|>']
+    T=Tokenizer.from_files(vocab_filepath,merges_filepath,special_tokens)
+    text="""I love kitty!"""
+    print(T.encode(text))
